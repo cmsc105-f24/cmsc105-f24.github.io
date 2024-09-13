d378f08df9689ba4e1062ded44d076728549d4f7
DavidBalash <david.balash@richmond.edu>
2024-09-13 13:38:31 -0400

    Updates

---
+++ labs/grades.py
@@ -0,0 +1,7 @@
+''' 
+Author name: <Your name here>
+This program counts the total of students that get specific grades.
+'''
+
+string_val = "User1:Grade-A, User2:Grade-B, User3:Grade-C, User4:Grade-A, User5:Grade-B, User6: Grade-A, User7: Grade-C, User8: Grade-B, User9:Grade-A, User10: Grade-A, User11: Grade-C, User12: Grade-A, User13: Grade-B, User14: Grade-C, User15: Grade-A"
+
